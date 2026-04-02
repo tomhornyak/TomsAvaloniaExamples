a6e841e00d2cb9f500f093d5bcd34ce3a002228a
tomhornyak <thomas.hornyak@gmail.com>
2026-04-02 17:57:28 -0400

    Added more debug info.

--- CanvasTest2/Views/MainWindow.axaml.cs
+++ CanvasTest2/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System.Diagnostics;
 
 namespace CanvasTest2.Views
 {
@@ -12,6 +13,12 @@ namespace CanvasTest2.Views
         private void GetCanvas_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             var tilesCanvas = TilesItemControl.ItemsPanelRoot as Canvas;
+            Debug.WriteLine($"Canvas found: Children count: {tilesCanvas?.Children.Count}");
+            foreach (var child in tilesCanvas?.Children ?? [])
+            {
+                Debug.WriteLine($"Child: {child}");
+            }
+
         }
     }
 }
